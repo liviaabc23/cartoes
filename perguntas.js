ad3b73f1e16e0629b0f3c82776633b78406398fe
liviaabc23 <168587313+liviaabc23@users.noreply.github.com>
2024-10-28 15:54:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Basquete',
+    'O que é um ala?',
+    'É um jogador responsável por marcar muitos pontos que também tem funções de defesa e focam em rebotes e passes.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um atacante?',
+    'É um jogador que atua no setor mais ofensivo. Ele deve conduzir a bola até a grande área do time adversário e marcar o gol.'
+)
+
+criaCartao(
+    'Vôlei',
+    'O que é um central?',
+    'É um jogador que ataca pelo meio da rede, bloqueia e é responsável pelas bolas rápidas.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um goleiro?',
+    'É um jogador que deve impedir que a equipe adversária marque pontos, ou seja, evitar o gol.'
+)
+
+criaCartao(
+    'Basquete',
+    'O que é um armador?',
+    'É um jogador responsável por organizar as jogadas de ataque e definir quais movimentos devem ser feitos até o arremesso.'
+)
+
+criaCartao(
+    'Vôlei',
+    'O que é um ponteiro?',
+    'É um jogador que atua como atacante e defensor.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um zagueiro?',
+    'É um jogador que deve defender a última linha de defesa do time, antes do goleiro.'
+)
+
+criaCartao(
+    'Vôlei',
+    'O que é um líbero?',
+    'É um jogador que não pode atacar, bloquear ou sacar, especializado em recepção e defesa.'
+)
+
+criaCartao(
+    'Basquete',
+    'O que é um pivô?',
+    'É um jogador que atua dentro do garrafão tanto no ataque quanto na defesa, sempre perto da cesta.'
+)
+
